934f4fa7b102c6481ffb66432737db60cd492e48
Srikanthreddyvvs <venkatasaisrikanthreddyvenna@gmail.com>
2026-07-23 13:15:12 +0530

    initial project setup from Spring Initializr

---
+++ src/main/java/com/DiffHawk/DiffHawkApplication.java
@@ -0,0 +1,13 @@
+package com.DiffHawk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffHawkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiffHawkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DiffHawk/DiffHawkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DiffHawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffHawkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
